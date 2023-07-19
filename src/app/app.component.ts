56ee2787519246fc9f52cdd0818bb482ae488499
Tomasz Sapinski <tomasz.sapinski.sp@lhsystems.com>
2023-07-19 20:58:16 +0200

    init repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwa-notification-frontend';
+
+  subscription: PushSubscription | undefined;
+
+  constructor(private readonly swPush: SwPush) {
+
+  }
+
+  subscribeToNotifications(): void {
+    this.swPush
+      .requestSubscription({
+        serverPublicKey: 'BNimwgn6C3Po9dQrX5KEj_4RQAqkIS3dHNSzrWDLOlmSdZ-DDjCU-jiBQCuGOsE2fCLvUitYmfpDqQlWBnDYUbU'
+      })
+      .then((sub) => this.subscription = sub)
+      .then(() => {
+        console.info('Subscribed to server');
+        navigator.serviceWorker.getRegistration().then((reg) => {
+          if (reg != null) {
+            reg.showNotification('Push notification activated');
+          }
+        });
+      })
+      .catch((err) => console.error('Could not subscribe to notifications', err));
+  }
+
 }
